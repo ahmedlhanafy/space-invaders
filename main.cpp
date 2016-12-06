67c4308768629481d2028fdeca6e4764e3c1dba6
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2016-12-06 17:54:00 +0200

    Create Coordinates, Rotation & Spaceship header files

--- main.cpp
+++ main.cpp
@@ -2,37 +2,13 @@
 
 #include <GL/glut.h>
 #include <stdio.h>
+#include "Coordinates.h"
+#include "Rotation.h"
+#include "Spaceship.h"
 
 // CONSTANTS
 
 #define PI 3.14159265
-
-// CLASSES
-
-class Coordinates {
-public:
-    float x, y, z;
-    Coordinates(float x, float y, float z);
-};
-
-Coordinates::Coordinates(float x, float y, float z) {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-}
-
-class Rotation {
-public:
-    float angle, x, y, z;
-    Rotation(float angle, float x, float y, float z);
-};
-
-Rotation::Rotation(float angle, float x, float y, float z) {
-    this->angle = angle;
-    this->x = x;
-    this->y = y;
-    this->z = z;
-}
 
 // FIXED CONFIGURATIONS
 
@@ -44,76 +20,86 @@ int WINDOW_POSITION_Y = 150;
 
 // VARIABLE CONFIGURATIONS
 
-Coordinates observerCoordinates(0, 0, 0);
+Coordinates observerCoordinates(0, 3, 5);
 Coordinates observedCoordinates(0, 0, 0);
 Coordinates mouseCoordinates(0, 0, 0);
+
+Spaceship player(0, 0, 2.5, 0, 0, 0, 0);
+Spaceship opponent(0, 0, -3, 0, 0, 0, 0);
 
 // CAMERA & LIGHTS
 
 void setupCamera() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0f, 300 / 300, 0.1f, 300.0f);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(45.0f, 300 / 300, 0.1f, 300.0f);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 
-    printf("Camera -> (%f, %f, %f)\n", observerCoordinates.x, observerCoordinates.y, observerCoordinates.z);
+  printf("Camera -> (%f, %f, %f)\n", observerCoordinates.x, observerCoordinates.y, observerCoordinates.z);
 
-    gluLookAt(observerCoordinates.x, observerCoordinates.y, observerCoordinates.z,
-              observedCoordinates.x,observedCoordinates.y , observedCoordinates.z,
-              0, 1, 0);
+  gluLookAt(observerCoordinates.x, observerCoordinates.y, observerCoordinates.z,
+            observedCoordinates.x,observedCoordinates.y , observedCoordinates.z,
+            0, 1, 0);
 }
 
 void setupLights() {
-    GLfloat ambient[] = { 0.7f, 1.0f, 0.7, 1.0f };
-    GLfloat diffuse[] = { 1.0f, 0.6f, 0.6, 1.0f };
-    GLfloat specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
-    GLfloat shininess[] = { 50 };
+  GLfloat ambient[] = { 0.7f, 1.0f, 0.7, 1.0f };
+  GLfloat diffuse[] = { 1.0f, 0.6f, 0.6, 1.0f };
+  GLfloat specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
+  GLfloat shininess[] = { 50 };
 
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+  glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
 
-    GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
-    GLfloat lightPosition[] = { 3.0f, 8.0f, 15.0f, 0.0f };
-    GLfloat lightDirection[] = { 0.0f, 0.0f, -1.0f, 0.0f };
-    glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
-    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, lightDirection);
+  GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
+  GLfloat lightPosition[] = { 3.0f, 8.0f, 15.0f, 0.0f };
+  GLfloat lightDirection[] = { 0.0f, 0.0f, -1.0f, 0.0f };
+  glLightfv(GL_LIGHT0, GL_POSITION, lightIntensity);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
+  glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, lightDirection);
 }
 
 // HANDLERS
 
 void mouseHandler(int x, int y) {
-    mouseCoordinates.x = x;
-    mouseCoordinates.y = WINDOW_HEIGHT - y;
+  mouseCoordinates.x = x;
+  mouseCoordinates.y = WINDOW_HEIGHT - y;
 
-    glutPostRedisplay();
+  glutPostRedisplay();
 }
 
 void keyboardHandler(unsigned char k, int x, int y) {
-    if(k == 'w')
-        observerCoordinates.z--;
-    if(k == 's')
-        observerCoordinates.z++;
-    if(k == 'r')
-        observerCoordinates.y++;
-    if(k == 'f')
-        observerCoordinates.y--;
-    if(k == 'a')
-        observerCoordinates.x--;
-    if(k == 'd')
-        observerCoordinates.x++;
+  if(k == 27)
+      exit(0);
+  if(k == 'w')
+      observerCoordinates.z--;
+  if(k == 's')
+      observerCoordinates.z++;
+  if(k == 'r')
+      observerCoordinates.y++;
+  if(k == 'f')
+      observerCoordinates.y--;
+  if(k == 'a')
+      observerCoordinates.x--;
+  if(k == 'd')
+      observerCoordinates.x++;
 
-    glutPostRedisplay();
+  glutPostRedisplay();
 }
 
 void specialKeyboardHandler(int k, int x, int y) {
-    glutPostRedisplay();
+  if(k == GLUT_KEY_RIGHT)
+    player.coordinates->x += 0.1;
+  if(k == GLUT_KEY_LEFT)
+    player.coordinates->x -= 0.1;
+
+  glutPostRedisplay();
 }
 
 // DISPLAY & ANIMATION
@@ -123,14 +109,20 @@ void display() {
   setupCamera();
 
   glPushMatrix();
-  glutSolidCube(5);
+  glTranslated(player.coordinates->x, player.coordinates->y, player.coordinates->z);
+  glutSolidCube(0.7);
+  glPopMatrix();
+
+  glPushMatrix();
+  glTranslated(opponent.coordinates->x, opponent.coordinates->y, opponent.coordinates->z);
+  glutSolidCube(0.4);
   glPopMatrix();
 
   glFlush();
 }
 
 void animation() {
-    glutPostRedisplay();
+  glutPostRedisplay();
 }
 
 // MAIN
